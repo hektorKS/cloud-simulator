25047b79138b6f58a23c95d118ed6f1ac81f31f9
Konrad Szymański <konrad.w.szymanski@student.put.poznan.pl>
2019-10-06 20:21:34 +0200

    Configurable seed for generator

--- src/bimodal/BimodalDistributionGenerator.py
+++ src/bimodal/BimodalDistributionGenerator.py
@@ -8,26 +8,27 @@ SEED_MAX_VALUE = 100000
 
 class BimodalDistributionGenerator:
 
-    @staticmethod
-    def __reset_numpy_random_state():
-        seed = random.randint(SEED_MIN_VALUE, SEED_MAX_VALUE)
-        np.random.seed(seed)
-
-    def __init__(self, number_of_elements: int, space_between_peaks: int, standard_deviation: int):
+    def __init__(self, number_of_elements: int, space_between_peaks: int, standard_deviation: int, seed: int = None):
         assert number_of_elements > 0
         assert space_between_peaks > 0
         assert standard_deviation > 0
         self.number_of_elements = number_of_elements
         self.space_between_peaks = space_between_peaks
         self.standard_deviation = standard_deviation
+        self.seed = seed
 
     def generate(self):
-        BimodalDistributionGenerator.__reset_numpy_random_state()
+        self.__reset_numpy_random_state()
         bimodal_distribution = np.concatenate((
             self.__generate_normal_distribution_of_half(int((self.number_of_elements - self.space_between_peaks) / 2)),
             self.__generate_normal_distribution_of_half(int((self.number_of_elements + self.space_between_peaks) / 2))
         ))
         return bimodal_distribution.astype(int)
+
+    def __reset_numpy_random_state(self):
+        if self.seed is None:
+            self.seed = random.randint(SEED_MIN_VALUE, SEED_MAX_VALUE)
+        np.random.seed(self.seed)
 
     def __generate_normal_distribution_of_half(self, mean: int):
         return np.random.normal(mean, self.standard_deviation, int(0.5 * self.number_of_elements))
