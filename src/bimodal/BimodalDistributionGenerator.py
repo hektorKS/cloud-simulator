84f6792edae00aa0c6d91ab1ffe2b874459eef69
Piotr <a21sturmer@op.pl>
2019-10-06 22:51:38 +0200

    Added parameter defining percentage of short elements

--- src/bimodal/BimodalDistributionGenerator.py
+++ src/bimodal/BimodalDistributionGenerator.py
@@ -8,28 +8,35 @@ SEED_MAX_VALUE = 100000
 
 class BimodalDistributionGenerator:
 
-    def __init__(self, number_of_elements: int, space_between_peaks: int, standard_deviation: int, seed: int = None):
+    def __init__(self, number_of_elements: int, space_between_peaks: int, standard_deviation: int,
+                 percentage_of_short_elements: float, seed: int = None):
         assert number_of_elements > 0
         assert space_between_peaks > 0
         assert standard_deviation > 0
+        assert percentage_of_short_elements >= 0.0
+        assert percentage_of_short_elements <= 1.0
         self.number_of_elements = number_of_elements
         self.space_between_peaks = space_between_peaks
         self.standard_deviation = standard_deviation
         self.seed = seed
+        self.percentage_of_short_elements = percentage_of_short_elements
 
     def generate(self):
         self.__reset_numpy_random_state()
         bimodal_distribution = np.concatenate((
-            self.__generate_normal_distribution_of_half(int((self.number_of_elements - self.space_between_peaks) / 2)),
-            self.__generate_normal_distribution_of_half(int((self.number_of_elements + self.space_between_peaks) / 2))
+            self.__generate_normal_distribution_of_half(int((self.number_of_elements - self.space_between_peaks) / 2),
+                                                        self.percentage_of_short_elements),
+            self.__generate_normal_distribution_of_half(int((self.number_of_elements + self.space_between_peaks) / 2),
+                                                        1.0 - self.percentage_of_short_elements)
         ))
         return bimodal_distribution.astype(int)
 
     def __reset_numpy_random_state(self):
         if self.seed is None:
-            self.seed = random.randint(SEED_MIN_VALUE, SEED_MAX_VALUE)
-        np.random.seed(self.seed)
+            seed = random.randint(SEED_MIN_VALUE, SEED_MAX_VALUE)
+        else:
+            seed = self.seed
+        np.random.seed(seed)
 
-    def __generate_normal_distribution_of_half(self, mean: int):
-        return np.random.normal(mean, self.standard_deviation, int(0.5 * self.number_of_elements))
-
+    def __generate_normal_distribution_of_half(self, mean: int, percentage_of_elements: float):
+        return np.random.normal(mean, self.standard_deviation, int(percentage_of_elements * self.number_of_elements))
--- src/main.py
+++ src/main.py
@@ -6,13 +6,15 @@ NUMBER_OF_HISTOGRAM_COLUMNS = 100
 NUMBER_OF_ELEMENTS = 1000
 SPACE_BETWEEN_PEAKS = 500
 STANDARD_DEVIATION = 100
+PERCENTAGE_OF_SHORT_ELEMENTS = 0.5
 
 
 def main():
     bimodal_distribution_generator = BimodalDistributionGenerator(
         NUMBER_OF_ELEMENTS,
         SPACE_BETWEEN_PEAKS,
-        STANDARD_DEVIATION
+        STANDARD_DEVIATION,
+        PERCENTAGE_OF_SHORT_ELEMENTS
     )
     bimodal_distribution = bimodal_distribution_generator.generate()
     HistogramDrawer.draw(bimodal_distribution, NUMBER_OF_HISTOGRAM_COLUMNS)
